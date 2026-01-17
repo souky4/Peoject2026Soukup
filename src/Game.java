e260af6a753ac68c3c4fa10f49c770a15de683ec
ondrej <soukup2@spsejecna.cz>
2026-01-17 21:36:32 +0100

    -Vytvoreni tridy Game

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,12 @@
 public class Game {
+    Private Hrac hrac;
+    Private SpravcePrikazu spravcePrikazu;
+    Private boolean hraBezi;
+
+    public void startGame(){};
+    public void vstup(String vstup){};
+    public void endGame(){};
+
+
 
 }
