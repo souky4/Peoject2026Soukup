19c60f82c1f4eef8deb61bde51654c652765fc5f
ondrej <soukup2@spsejecna.cz>
2026-02-01 20:03:51 +0100

    vytvoreni tridy World

--- src/World.java
+++ src/World.java
@@ -1,2 +1,13 @@
+import Hrac.Room;
+
+import java.util.Map;
+
 public class World {
+    private Map<String, Room> rooms;
+    private Room startingRoom;
+
+    public void addRoom(Room room) {};
+    public Room getRoom(String roomName) {};
+    public void setStartingRoom(Room room){}
+    public Room getStartingRoom(){};
 }
