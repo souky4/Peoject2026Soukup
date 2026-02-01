d7517665ab62056909a55466ef784136ac6d4765
ondrej <soukup2@spsejecna.cz>
2026-02-01 19:57:51 +0100

    vytvoreni tridy WorldLoader

--- src/Hrac/Room.java
+++ src/Hrac/Room.java
@@ -13,11 +13,12 @@ public class Room {
     private ArrayList<Character> characters;
 
     public String getJmeno() {};
+    public String getPopis {};
     public Room getExit(String smer) {};
     public void addExit(String smer, Room room){};
     public void addItem (Item item){};
     public void removeItem(Item item){};
     public void popis(){};
-
-
+    public ArrayList<Item> getItems(){};
+    public ArrayList<Character> getCharacters() {};
 }
---
+++ src/WorldLoader.java
@@ -0,0 +1,12 @@
+public class WorldLoader {
+    public World loadWorld(String nazevSlozky){};
+
+    private void loadRooms(World world,Object json){};
+    private void connectRooms(World world, Object json){};
+    private void loadItems(World world, Object json){};
+    private void loadCharacters(World world, Object json){};
+    private void setStartingRoom(World world, Object json){};
+
+
+
+}
