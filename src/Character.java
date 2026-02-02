5bfbfa089a2189305e572787395167a87b5ca108
ondrej <soukup2@spsejecna.cz>
2026-02-02 16:32:45 +0100

    upraveni tridy Character

---
+++ src/Character.java
@@ -0,0 +1,23 @@
+public class Character {
+    private String jmeno;
+    private String popis;
+    private String dialog;
+
+    public Character(String jmeno, String popis, String dialog) {
+        this.jmeno = jmeno;
+        this.popis = popis;
+        this.dialog = dialog;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public String getDialog() {
+        return dialog;
+    }
+}
